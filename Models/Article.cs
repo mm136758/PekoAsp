8b96adcc3f652955134a222d3860d40f017df766
mirika <mm136758@gmail.com>
2022-08-14 21:29:22 +0800

    1. change data view 2.change url route

--- Models/Article.cs
+++ Models/Article.cs
@@ -8,7 +8,9 @@ namespace PekoAsp.Models
 		public int ID { get; set; }
 		public string Title { get; set; }
 
+		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
 		public DateTime ReleaseDate { get; set; }
 		public string Link { get; set; }
 		public decimal Count { get; set; }
