a53117e4abcd4c141bf57c23b07b43eb76d092f2
mirika <mm136758@gmail.com>
2022-08-15 22:10:02 +0800

    Update Article

--- Models/Article.cs
+++ Models/Article.cs
@@ -6,14 +6,19 @@ namespace PekoAsp.Models
 	public class Article
 	{
 		public int ID { get; set; }
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
 		public string Title { get; set; }
 
 		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
 		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+		[Required]
 		public DateTime ReleaseDate { get; set; }
 		public string Link { get; set; }
 		public decimal Count { get; set; }
+		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+		[StringLength(30)]
 		public string Category { get; set; }
 	}
 }
