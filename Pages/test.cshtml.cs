d2a8719e2cbaa3e283c9242ceb57c676e4e7defe
mirika <mm136758@gmail.com>
2022-08-17 13:24:57 +0800

    del app setting

---
+++ Pages/test.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PekoAsp.Data;
+using PekoAsp.Models;
+
+namespace PekoAsp.Pages
+{
+    public class testModel : PageModel
+    {
+        private readonly PekoAsp.Data.PekoAspContext _context;
+
+        public testModel(PekoAsp.Data.PekoAspContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Article> Article { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Article != null)
+            {
+                Article = await _context.Article.ToListAsync();
+            }
+        }
+    }
+}
